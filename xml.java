79ea408334f13858eea9c433aac26b1a0b741f03
Suresh Medisetti <156284622+Suresh-Medisetti@users.noreply.github.com>
2024-11-27 21:10:10 +0530

    Update xml.java

--- xml.java
+++ xml.java
@@ -1,48 +1,44 @@
 import java.io.File;
-import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.*;
 import java.util.Scanner;
 
- class UserDom {
+
+public class UserDom {
     public static void main(String[] args) throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(new File("s.xml"));
-        doc.getDocumentElement().normalize();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter User ID:");
-        int userIdInput = scanner.nextInt();
-        boolean userFound = false;
-
-        NodeList users = doc.getElementsByTagName("user");
-        for (int i = 0; i < users.getLength(); i++) {
-            Node node = users.item(i);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element userElement = (Element) node;
-                int userId = Integer.parseInt(
-                    userElement.getElementsByTagName("userid").item(0).getTextContent()
-                );
-                if (userId == userIdInput) {
-                    System.out.println("User Details:");
-                    System.out.println("UserID: " + 
-                        userElement.getElementsByTagName("userid").item(0).getTextContent());
-                    System.out.println("Username: " + 
-                        userElement.getElementsByTagName("username").item(0).getTextContent());
-                    System.out.println("Address: " + 
-                        userElement.getElementsByTagName("address").item(0).getTextContent());
-                    System.out.println("Phone: " + 
-                        userElement.getElementsByTagName("phone").item(0).getTextContent());
-                    System.out.println("Email: " + 
-                        userElement.getElementsByTagName("email").item(0).getTextContent());
-                    userFound = true;
-                    break;
-                }
-            }
-        }
-
-        if (!userFound) {
-            System.out.println("User not available.");
+    // Load and parse the XML file
+          DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+          DocumentBuilder builder = factory.newDocumentBuilder();
+          Document document = builder.parse(new File("s.xml"));
+          document.getDocumentElement().normalize();
+          // Input User ID
+          Scanner scanner = new Scanner(System.in);
+          System.out.print("Enter User ID: ");
+          int userId = scanner.nextInt();
+          // Fetch user details
+          NodeList userList = document.getElementsByTagName("user");
+          boolean userFound = false;
+          for (int i = 0; i < userList.getLength(); i++) {
+          Element user = (Element) userList.item(i);
+          int id = Integer.parseInt(user.getElementsByTagName("userid").item(0).getTextContent());
+          if (id == userId) {
+               System.out.println("User Details:");
+               System.out.println("ID: " + id);
+               System.out.println("Name: " + 
+               user.getElementsByTagName("username").item(0).getTextContent());
+               System.out.println("Address: " + 
+               user.getElementsByTagName("address").item(0).getTextContent());
+               System.out.println("Phone: " + user.getElementsByTagName("phone").item(0).getTextContent());
+               System.out.println("Email: " + user.getElementsByTagName("email").item(0).getTextContent());
+               userFound = true;
+               break;
+              }
+          }
+          if (!userFound) {
+             System.out.println("User not available.");
+           }
+          scanner.close();
         }
     }
-}
+    
